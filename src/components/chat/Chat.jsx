722cafaf433ed71a0f6f5878d6fec021271531a9
zeynebbesra <394777@ogr.ktu.edu.tr>
2024-10-04 21:16:37 +0300

    chat message design created

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,147 @@
+import { useState } from "react"
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
+
+  const handleEmoji = e =>{
+    setText((prev) => prev + e.emoji)
+    setOpen(false)
+  }
+
+  console.log(text)
+
+
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet adipisicing elit.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <img src="./avatar.png" alt="" /> */}
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <img src="./avatar.png" alt="" /> */}
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <img src="./avatar.png" alt="" /> */}
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <img src="./avatar.png" alt="" /> */}
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Animi repellat ad, maiores maxime voluptate fuga autem cum soluta aliquid sit odit eligendi labore velit 
+              iusto et quam veritatis odio qui officiis placeat, totam hic sapiente consectetur laudantium? 
+              Quibusdam eveniet maxime quae pariatur quod, alias, fugit, in tenetur excepturi ducimus deserunt.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input 
+          type="text" 
+          placeholder="Type a message..." 
+          value={text}
+          onChange={e=>setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)}/>
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji}/> 
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
   )
 }
 
